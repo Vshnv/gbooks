a30fcb59a816de730ff921545aeefebdfb0a6398
Vshnv <vaishnavanil7th@gmail.com>
2022-07-01 13:41:01 +0530

    Removed unused Color#random extension

--- gbooks/BookCategoryCollectionViewController/BookCategoryCollectionViewController.swift
+++ gbooks/BookCategoryCollectionViewController/BookCategoryCollectionViewController.swift
@@ -121,12 +121,3 @@ class BookCategoryCollectionViewController: UICollectionViewController {
     }
     
 }
-
-extension UIColor {
-    static var random: UIColor {
-        return UIColor(red: .random(in: 0...1),
-                       green: .random(in: 0...1),
-                       blue: .random(in: 0...1),
-                       alpha: 0.7)
-    }
-}
