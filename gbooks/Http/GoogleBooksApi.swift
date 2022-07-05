f6a4444ebc10baac236da3508c17ae5b101d7b2c
Vshnv <vaishnavanil7th@gmail.com>
2022-07-05 16:17:19 +0530

    Commented GoogleBooksAPI

--- gbooks/Http/GoogleBooksApi.swift
+++ gbooks/Http/GoogleBooksApi.swift
@@ -2,9 +2,25 @@ import Foundation
 
 
 /**
- * Exposes google books API
+ * Interfaces the Google Books API (https://developers.google.com/books)
  */
 protocol GoogleBooksApi {
+    /**
+     * Fetches volumes based on given query and subject
+     *
+     * - warning: if query is nil, subject must not be .none and vice versa
+     * - parameter query: query string for title of book
+     * - parameter subject: subject of book to be searched
+     * - parameter startIndex: offset to actual query result from where response starts
+     * - parameter maxResults: maximum number of volumes to be included in the response
+     * - returns: volumes along with a some extra information about the results of the given query
+     */
     func fetchVolumes(_ query: String?, subject: Subject, startIndex: Int, maxResults: Int) async throws -> VolumesFetchResult
+    /**
+     * Fetches a volume matching the specific isbn code
+     *
+     * - parameter isbn: unique isbn code of required book
+     * - returns: fetch result with either a single or empty item collection depending on validity of isbn code
+     */
     func fetchVolumes(byIsbn isbn: String) async throws -> VolumesFetchResult
 }
